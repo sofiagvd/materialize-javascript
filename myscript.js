0dba503204bfe0f4fca11d974ec02a2ae07b2740
Sofia Giselle <47372952+sofiagvd@users.noreply.github.com>
2019-05-13 21:07:32 -0400

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    //this function will run after the page has finished loading.
+    console.log("Page has loaded.")
+}
+ $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
